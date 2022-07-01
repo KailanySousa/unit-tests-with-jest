477237bb735eb62d031c7023617967226cf9bed4
KailanySousa <kailany-sousa2010@hotmail.com>
2022-06-30 21:35:27 -0300

    feat: create tests

---
+++ aula01/__tests__/calculator.spec.js
@@ -0,0 +1,15 @@
+import { sum } from "../calculator.js";
+
+describe("calculator sum", () => {
+  test("it should sum two positive values", () => {
+    const result = sum(2, 2);
+
+    expect(result).toBe(4);
+  });
+
+  test("it should sum numbers with a negative value", () => {
+    const result = sum(2, -2);
+
+    expect(result).toBe(0);
+  });
+});
